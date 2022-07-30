8bcc845f5f2c13ccbfa077222b72a684e26f7a17
Gen Kawamura <64403862+kawamuragen@users.noreply.github.com>
2022-07-30 20:50:01 +0900

    Up to lesson 71

---
+++ src/components/templates/HeaderLayout.tsx
@@ -0,0 +1,18 @@
+import { memo, ReactNode, VFC } from "react";
+
+import { Header } from "../organisms/Layout/Header";
+
+// タグで囲ったものを受け取るときにReactNode使う
+type Props = {
+  children: ReactNode;
+};
+
+export const HeaderLayout: VFC<Props> = memo((props) => {
+  const { children } = props;
+  return (
+    <>
+      <Header />
+      {children}
+    </>
+  );
+});
