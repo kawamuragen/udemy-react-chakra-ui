1fbfba64e1ca8826cca54249d96c2a99ca058b3e
Gen Kawamura <64403862+kawamuragen@users.noreply.github.com>
2022-08-19 09:58:38 +0900

    Up to lesson 78.

--- src/components/pages/UserManagement.tsx
+++ src/components/pages/UserManagement.tsx
@@ -1,5 +1,20 @@
 import { memo, VFC } from "react";
+import { Wrap, WrapItem } from "@chakra-ui/react";
+
+import { UserCard } from "../organisms/user/UserCard";
 
 export const UserManagement: VFC = memo(() => {
-  return <p>ユーザ管理画面です</p>;
+  return (
+    <Wrap spacing="8px" p={{ base: 4, md: 10 }}>
+      {[...Array(10)].map(() => (
+        <WrapItem>
+          <UserCard
+            imageUrl="https://source.unsplash.com/random"
+            userName="じゃけえ"
+            fullName="takumi okada"
+          />
+        </WrapItem>
+      ))}
+    </Wrap>
+  );
 });
