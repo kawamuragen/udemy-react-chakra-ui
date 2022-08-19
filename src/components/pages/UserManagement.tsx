935bc67c792fb77b98f1a7e14659ba820fe5c275
Gen Kawamura <64403862+kawamuragen@users.noreply.github.com>
2022-08-19 10:41:49 +0900

    Up to lesson 79.

--- src/components/pages/UserManagement.tsx
+++ src/components/pages/UserManagement.tsx
@@ -1,20 +1,37 @@
-import { memo, VFC } from "react";
-import { Wrap, WrapItem } from "@chakra-ui/react";
+/* eslint-disable react-hooks/exhaustive-deps */
+import { memo, useEffect, VFC } from "react";
+import { Center, Spinner, Wrap, WrapItem } from "@chakra-ui/react";
 
 import { UserCard } from "../organisms/user/UserCard";
+import { useAllUsers } from "../../hooks/useAllUsers";
 
+// https://chakra-ui.com/docs/components/wrap/usage
+// Wrap 自動でレスポンシブ対応してくれる機能
 export const UserManagement: VFC = memo(() => {
+  const { getUsers, users, loading } = useAllUsers();
+
+  // 画面遷移初回のみ呼び出したいので useEffect の空配列をいれる
+  useEffect(() => getUsers(), []);
+
   return (
-    <Wrap spacing="8px" p={{ base: 4, md: 10 }}>
-      {[...Array(10)].map(() => (
-        <WrapItem>
-          <UserCard
-            imageUrl="https://source.unsplash.com/random"
-            userName="じゃけえ"
-            fullName="takumi okada"
-          />
-        </WrapItem>
-      ))}
-    </Wrap>
+    <>
+      {loading ? (
+        <Center h="100vh">
+          <Spinner />
+        </Center>
+      ) : (
+        <Wrap justify="space-around" spacing="8px" p={{ base: 4, md: 10 }}>
+          {users.map((user) => (
+            <WrapItem key={user.id}>
+              <UserCard
+                imageUrl="https://source.unsplash.com/random"
+                userName={user.username}
+                fullName={user.name}
+              />
+            </WrapItem>
+          ))}
+        </Wrap>
+      )}
+    </>
   );
 });
